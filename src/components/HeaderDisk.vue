4ed2a31e324ecb1c461c7fbae45f8fa9fc2e1fc6
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-24 17:18:33 +0100

    header done

--- src/components/HeaderDisk.vue
+++ src/components/HeaderDisk.vue
@@ -1,10 +1,12 @@
 <template>
   <header class="d-flex justify-content-between">
     <div><img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="spotify logo"></div>
-    <select name="" id="">
-      <option value="1">Rock</option>
-      <option value="2">Metal</option>
-      <option value="3">Jazz</option>
+    <select name="select_genre" id="select_genre">
+      <option selected>All genres</option>
+      <option value="rock">Rock</option>
+      <option value="metal">Metal</option>
+      <option value="jazz">Jazz</option>
+      <option value="pop">Pop</option>
     </select>
   </header>
 </template>
